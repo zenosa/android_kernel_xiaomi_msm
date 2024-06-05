66053dad51f6818ae53c55d4eb9897be4311f006
Benarji Anand <benarji385@gmail.com>
2024-06-05 16:53:03 +0800

    drm: msm: Retrieve mdp node from parent mdss
    In the current implementation, substring
    comparison using device node name is used
    to find mdp node during driver probe. Use
    compatible string list instead to get mdp
    node from the parent mdss node.
    
    Signed-off-by: Krishna Manikandan <mkrishn@codeaurora.org>
    Signed-off-by: Benarji Anand <benarji385@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1231,9 +1231,13 @@ static int add_components_mdp(struct device *mdp_dev,
 	return 0;
 }
 
-static int compare_name_mdp(struct device *dev, void *data)
+static int find_mdp_node(struct device *dev, void *data)
 {
-	return (strnstr(dev_name(dev), "mdp") != NULL);
+	if (!dev->driver)
+		return 0;
+
+	return (of_match_node(dev->driver->of_match_table,
+				dev->of_node) != NULL);
 }
 
 static int add_display_components(struct device *dev,
@@ -1256,7 +1260,7 @@ static int add_display_components(struct device *dev,
 			return ret;
 		}
 
-		mdp_dev = device_find_child(dev, NULL, compare_name_mdp);
+		mdp_dev = device_find_child(dev, NULL, find_mdp_node);
 		if (!mdp_dev) {
 			dev_err(dev, "failed to find MDSS MDP node\n");
 			of_platform_depopulate(dev);
